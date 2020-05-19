76e20a1a2616bc6a415fbb4683a84575d8b292b6
Cameron Fearrington <camfearrington@gmail.com>
2020-05-19 03:13:20 -0400

    Initial commit

---
+++ website/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+from django.core.mail import send_mail
+
+def home(request):
+	return render(request,'home.html', {})
+
+def about(request):
+	return render(request,'about.html', {})
+
+def blogdetails(request):
+	return render(request,'blog-details.html', {})
+
+def blog(request):
+	return render(request,'blog.html', {})
+
+def contact(request):
+	if request.method == "POST":
+		message_name = request.POST['message-name']
+		message_email = request.POST['message-email']
+		message = request.POST['message']
+
+		#send email
+		send_mail(
+			message_name, #subject
+			message, #message
+			message_email, #from email
+			['camfearrington@gmail.com'], #to email
+			fail_silently=False,
+			) 
+		return render(request, 'contact.html', {'message_name': message_name})
+	else:
+		return render(request,'contact.html', {})
+
+def pricing(request):
+	return render(request,'pricing.html', {})
+
+def service(request):
+	return render(request,'service.html', {})
